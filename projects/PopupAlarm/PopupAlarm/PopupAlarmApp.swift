a2158f075592b40c8b4020e28fedda27c61d4c4f
Jacob David C. Cunningham <jcun4128@gmail.com>
2022-09-04 14:07:01 -0500

    start new project

---
+++ projects/PopupAlarm/PopupAlarm/PopupAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopupAlarmApp.swift
+//  PopupAlarm
+//
+//  Created by Jacob David Cunningham on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct PopupAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
